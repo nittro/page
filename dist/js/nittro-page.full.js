0d558c2fe89233d64f5fc34dbd114b92e6c2e404
danik <me@subsonic.cz>
2016-04-04 22:03:42 +0200

    updated builds & deps to core v.1.9.6 which contains an important fix

--- dist/js/nittro-page.full.js
+++ dist/js/nittro-page.full.js
@@ -2232,7 +2232,7 @@ _context.invoke('Utils', function(Strings, undefined) {
 ;
 _context.invoke('Utils', function (Arrays, Strings, undefined) {
 
-    var map = function (args, callback) {
+    function map(args, callback) {
         args = Arrays.createFrom(args);
 
         if (Arrays.isArray(args[0])) {
@@ -2261,15 +2261,19 @@ _context.invoke('Utils', function (Arrays, Strings, undefined) {
 
             }
         }
-    };
+    }
 
-    var getElem = function (elem) {
-        Arrays.isArrayLike(elem) && elem !== window && (elem = elem[0]);
+    function getElem(elem) {
+        if (Arrays.isArray(elem) || elem instanceof HTMLCollection || elem instanceof NodeList) {
+            elem = elem[0];
+
+        }
+
         return typeof elem === 'string' ? DOM.getById(elem) : elem;
 
-    };
+    }
 
-    var getPrefixed = function (elem, prop) {
+    function getPrefixed(elem, prop) {
         if (Arrays.isArray(elem)) {
             elem = elem[0];
 
@@ -2294,9 +2298,9 @@ _context.invoke('Utils', function (Arrays, Strings, undefined) {
 
         return prop;
 
-    };
+    }
 
-    var parseData = function (value) {
+    function parseData(value) {
         if (!value) return null;
 
         try {
@@ -2306,7 +2310,7 @@ _context.invoke('Utils', function (Arrays, Strings, undefined) {
             return value;
 
         }
-    };
+    }
 
     var DOM = {
         getByClassName: function (className, context) {
